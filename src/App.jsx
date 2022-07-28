34b25f588791361827c80523acf6917f09226384
Mitchell Miller <mitchell.miller@arcurve.com>
2022-07-28 10:51:15 -0600

    This project was insanely easy

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,24 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+
 import './App.css'
+import useSound from 'use-sound'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [playBuzz] = useSound("/bzzz.mp3") 
+  const [playScream] = useSound("/scream.mp3") 
+
+  const handleClick = () =>{
+    let num = Math.floor(Math.random() * 21);
+    if(num == 20){
+      playScream();
+    }
+    playBuzz();
+  }
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button onClick={() => handleClick()}>Click Me!</button>
     </div>
   )
 }
